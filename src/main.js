676cd647aa5ba895d33c59b6d2fd297a5769df92
Leskhan.Karatayev2 <leskhan.karatayev2@homecredit.kz>
2021-10-11 15:41:50 +0600

    committed

--- src/main.js
+++ src/main.js
@@ -1,4 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import { Quasar } from 'quasar'
+import 'quasar/dist/quasar.sass'
+import quasarUserOptions from './quasar-user-options'
 
-createApp(App).mount('#app')
+createApp(App).use(Quasar, quasarUserOptions).mount('#app')
